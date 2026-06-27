97791bac8f9b7e063183cf4819e8b5b82237b588
Adam <ANONYM@ANONYM>
2026-06-27 20:24:38 +0200

    add: new script for invalid firewall rules

--- firewallCleanup.ps1
+++ firewallCleanup.ps1
@@ -2,11 +2,12 @@ param(
     [switch]$y = $false
 )
 
+Write-Output "Invalid Firewall Rules:"
 Get-NetFirewallApplicationFilter | ForEach-Object {
     $program = $_.Program
     if ([System.IO.Path]::IsPathRooted($program)) {
         if (!(Test-Path -Path "$program" -PathType Leaf)) {
-            Write-Output "Removing: $program"
+            Write-Output "$program"
             if ($y) {
                 $_ | Remove-NetFirewallRule
             }
